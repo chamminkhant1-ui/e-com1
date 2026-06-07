d7d3b882321ab175d9c7ffef315526d5371988c0
ChanMinKhant <jiyonfire@gmail.com>
2026-06-07 10:54:44 +0630

    vercel

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    // timeout after 3 seconds
-    await mongoose.connect(process.env.MONGO_URI, {
-        serverSelectionTimeoutMS: 3000,
-    });
-    console.log('MongoDB connected');
+  // timeout after 3 seconds
+  await mongoose.connect(process.env.MONGO_URI);
+  console.log('MongoDB connected');
 };
 
 module.exports = connectDB;
